a6b4991ed26faf7c32bfa6a5a918ebe2feb02c05
doccykins <doccykins@gmail.com>
2020-12-08 20:50:42 +0000

    Day 4 part 1 Solution

---
+++ day4/part1.ps1
@@ -0,0 +1,27 @@
+﻿$passports = Get-Content ".\input.txt" -Delimiter "`n`n";
+
+$passcount = 0;
+
+$valid = 0;
+
+foreach ($passport in $passports) {
+    
+     if (($passport -match "byr:") -and ($passport -match "iyr:") -and ($passport -match "eyr:") -and ($passport -match "hgt:") -and ($passport -match "hcl:") -and ($passport -match "ecl:") -and ($passport -match "pid:")) {
+        
+        $valid++;
+
+        Write-Output ("Entry " + $passcount + " VALID (match # " + $valid + ") " + $passport);        
+
+     } else {
+
+     Write-Output ("Entry " + $passcount + " invalid: " + $passport);
+     }
+
+
+  # Write-Output ("Entry " + $passcount + " " + $passport);
+  
+    $passcount++;
+
+}
+
+Write-Output ("Valid Passports: " + $valid);
